9d93ceb2a2e724421d980f5ca7e35756bb4e92f0
Mojek Rychly <rychlym@atlas.cz>
2018-02-21 14:35:36 +0100

    DateTimeUt

--- sample/ConsoleDemo/DateTimeUt.cs
+++ sample/ConsoleDemo/DateTimeUt.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 namespace ConsoleDemo
 {
-    public class DateTimeUt
+    public static class DateTimeUt
     {
         // like ICultureInfo.InvariantCulture.DateTimeFormat.UniversalSortableDateTimePattern
         public const string ISO8601LocalDateTime = "yyyy'-'MM'-'dd'T'HH':'mm':'ss";
@@ -23,12 +23,21 @@ namespace ConsoleDemo
         /// <summary>The short UTC date time (till the minutes) format</summary>
         public const string NoSepMnUtcDateTimeFormat = NoSepMnLocalDateTimeFormat + "Z";
 
-        public static string ToInvariant(DateTime dt, string dtFormat = ISO8601UtcDateTime) => dt.ToString(dtFormat, CultureInfo.InvariantCulture);
+        public static string ToInvariant(DateTime dt, string dtFormat = ISO8601LocalDateTime) => dt.ToString(dtFormat, CultureInfo.InvariantCulture);
+        public static string ToUtcInvariant(DateTime dt, string dtFormat = ISO8601UtcDateTime) => dt.ToUniversalTime().ToString(dtFormat, CultureInfo.InvariantCulture);
 
-        public static bool TryParseExact(string dateStr, string dateFormat, out DateTime dateTime)
-        {
-            return DateTime.TryParseExact(dateStr, dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime);
-        }
+        public static bool TryParseExact(string dateTimeStr, string dateTimeFormat, out DateTime dateTime)
+            => DateTime.TryParseExact(dateTimeStr, dateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime);
+
+        public static bool TryParseExactUtc(string utcDateTimeStr, string utcDateTimeFormat, out DateTime dateTime)
+            => DateTime.TryParseExact(utcDateTimeStr, utcDateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal, out dateTime);
+
+        public static bool TryParseExactLocal(string utcDateTimeStr, string utcDateTimeFormat, out DateTime dateTime)
+            => DateTime.TryParseExact(utcDateTimeStr, utcDateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal, out dateTime);
+
+        public static DateTime ParseExactToLocalDateTime(string ltDateTimeStr, string localDateTimeFormat = ISO8601LocalDateTime)
+            => DateTime.ParseExact(ltDateTimeStr, localDateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal).ToLocalTime();
+
 
         public static string ConvertExactDateFormat(string inputDateStr, string inputFormat, string outputFormat, string parseFailOutput = "-")
         {
@@ -37,6 +46,50 @@ namespace ConsoleDemo
                 return dateTime.ToString(outputFormat);
             }
             return parseFailOutput;
+        }
+
+
+        /// <summary>
+        /// Tries to parse the given datetime string that is not annotated with a timezone 
+        /// information but known to be in the CET/CEST zone and returns a DateTime struct
+        /// in UTC (so it can be converted to the devices local time). If it could not be 
+        /// parsed, result contains the current date/time in UTC.
+        /// </summary>
+        public static bool TryParseCetCestToUtc(string s, string isoFormat, out DateTime result)
+        {
+            // Parse datetime, knowing it is in CET/CEST timezone. Parse as universal as we fix it afterwards
+            if (s.EndsWith(" CET")) s = s.Substring(0, s.Length - 4);
+            else if (s.EndsWith(" CEST")) s = s.Substring(0, s.Length - 5);
+
+            if (!DateTime.TryParseExact(s, isoFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out result))
+            {
+                result = DateTime.UtcNow;
+                return false;
+            }
+            result = DateTime.SpecifyKind(result, DateTimeKind.Utc);
+
+            // The boundaries of the daylight saving time period in CET and CEST (_not_ in UTC!)
+            // Both DateTime structs are of kind 'Utc', to be able to compare them with the parsing result
+            DateTime DstStart = LastSundayOf(result.Year, 3).AddHours(2);
+            DateTime DstEnd = LastSundayOf(result.Year, 10).AddHours(3);
+
+            // Are we inside the daylight saving time period?
+            if (DstStart.CompareTo(result) <= 0 && result.CompareTo(DstEnd) < 0)
+                result = result.AddHours(-2); // CEST = UTC+2h
+            else
+                result = result.AddHours(-1); // CET = UTC+1h
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns the last Sunday of the given month and year in UTC
+        /// </summary>
+        private static DateTime LastSundayOf(int year, int month)
+        {
+            DateTime firstOfNextMonth = new DateTime(year, month + 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return firstOfNextMonth.AddDays(firstOfNextMonth.DayOfWeek == DayOfWeek.Sunday ? -7 :
+                                                        (-1 * (int)firstOfNextMonth.DayOfWeek));
         }
 
         public static bool TryParseDateTimeFromSharePointProperty(string propertyValue, out DateTime dateTime)
@@ -66,4 +119,6 @@ namespace ConsoleDemo
             return false;
         }
     }
+
 }
+
